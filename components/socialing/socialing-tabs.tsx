2da935a2a31f47a8a989f3186e5f582e3f864c3b
JHyun Ahn <jhyunahn226@gmail.com>
2025-08-21 17:46:11 +0900

    소셜링 내부 탭 디자인 변경

--- components/socialing/socialing-tabs.tsx
+++ components/socialing/socialing-tabs.tsx
@@ -3,37 +3,27 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function SocialingTabs() {
   const pathname = usePathname();
 
-  // activeTab을 결정하는 로직은 동일하게 유지
   const activeTab = pathname.split("/")[2] || "meetup";
 
   return (
-    <div className="w-full h-8 md:h-auto flex-grow flex justify-center items-center text-base font-semibold">
-      <Link
-        href="/socialing/meetup"
-        className={cn(
-          "flex-1 text-center py-1 px-4 hover:text-primary hover:font-bold md:flex-none md:text-left md:py-4",
-          activeTab === "meetup"
-            ? "font-bold text-primary border-b-2 border-primary"
-            : "text-gray-400"
-        )}
-      >
-        Meetup
-      </Link>
-      <Link
-        href="/socialing/club"
-        className={cn(
-          "flex-1 text-center py-1 px-4 hover:text-primary hover:font-bold md:flex-none md:text-left md:py-4",
-          activeTab === "club"
-            ? "font-bold text-primary border-b-2 border-primary"
-            : "text-gray-400"
-        )}
-      >
-        Club
-      </Link>
-    </div>
+    <Tabs defaultValue={activeTab} className="mx-auto max-w-fit">
+      <TabsList className="flex justify-center items-center gap-2">
+        <TabsTrigger value="meetup" asChild>
+          <Link href="/socialing/meetup" className="px-4 py-2 md:px-6 md:py-4">
+            밋업
+          </Link>
+        </TabsTrigger>
+        <TabsTrigger value="club" asChild>
+          <Link href="/socialing/club" className="px-4 py-2 md:px-6 md:py-4">
+            클럽
+          </Link>
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
   );
 }
