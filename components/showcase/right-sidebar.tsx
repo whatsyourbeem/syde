11ef3e41261b087d116238063823480a5e5be739
SBS <bumsoo64@naver.com>
2025-12-22 17:19:42 +0900

    251222 right-sidebar copyright, 공유 삭제

--- components/showcase/right-sidebar.tsx
+++ components/showcase/right-sidebar.tsx
@@ -12,47 +12,6 @@ export function ShowcaseRightSidebar() {
 
         {/* Award Section */}
         <AwardSection />
-
-        {/* Footer Links */}
-        <div className="flex flex-col items-center text-center mt-8 pt-6 border-t border-gray-100">
-          <p className="text-sm text-gray-500 mb-4">ⓒ 2025. SYDE</p>
-          <div className="flex justify-center gap-4">
-            <Link
-              href="https://open.kakao.com/o/gduSGmtf"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="opacity-60 hover:opacity-100 transition-opacity"
-            >
-              <Image
-                src="/kakao-talk-bw.png"
-                alt="Kakao"
-                width={20}
-                height={20}
-              />
-            </Link>
-            <Link
-              href="https://www.instagram.com/syde.kr"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="opacity-60 hover:opacity-100 transition-opacity"
-            >
-              <Image
-                src="/instagram.png"
-                alt="Instagram"
-                width={20}
-                height={20}
-              />
-            </Link>
-            <Link
-              href="https://www.threads.com/@syde.kr"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="opacity-60 hover:opacity-100 transition-opacity"
-            >
-              <Image src="/threads.png" alt="Threads" width={20} height={20} />
-            </Link>
-          </div>
-        </div>
       </div>
     </div>
   );
