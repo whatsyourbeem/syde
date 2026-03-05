b16494fdaf7217476c22a89df02dc5d009ef56a0
JHyun Ahn <jhyunahn226@gmail.com>
2026-03-05 17:26:31 +0900

    폰트 가변화를 통한 성능 향상 (ttf->woff2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,32 +38,23 @@ export const viewport = {
 };
 
 const pretendard = localFont({
-  src: [
-    { path: "../fonts/Pretendard-Thin.ttf", weight: "100" },
-    { path: "../fonts/Pretendard-ExtraLight.ttf", weight: "200" },
-    { path: "../fonts/Pretendard-Light.ttf", weight: "300" },
-    { path: "../fonts/Pretendard-Regular.ttf", weight: "400" },
-    { path: "../fonts/Pretendard-Medium.ttf", weight: "500" },
-    { path: "../fonts/Pretendard-SemiBold.ttf", weight: "600" },
-    { path: "../fonts/Pretendard-Bold.ttf", weight: "700" },
-    { path: "../fonts/Pretendard-ExtraBold.ttf", weight: "800" },
-    { path: "../fonts/Pretendard-Black.ttf", weight: "900" },
-  ],
+  src: "../fonts/PretendardVariable.woff2",
   variable: "--font-pretendard",
   display: "swap",
+  weight: "45 920",
 });
 
 const paperlogy = localFont({
   src: [
-    { path: "../fonts/Paperlogy-1Thin.ttf", weight: "100" },
-    { path: "../fonts/Paperlogy-2ExtraLight.ttf", weight: "200" },
-    { path: "../fonts/Paperlogy-3Light.ttf", weight: "300" },
-    { path: "../fonts/Paperlogy-4Regular.ttf", weight: "400" },
-    { path: "../fonts/Paperlogy-5Medium.ttf", weight: "500" },
-    { path: "../fonts/Paperlogy-6SemiBold.ttf", weight: "600" },
-    { path: "../fonts/Paperlogy-7Bold.ttf", weight: "700" },
-    { path: "../fonts/Paperlogy-8ExtraBold.ttf", weight: "800" },
-    { path: "../fonts/Paperlogy-9Black.ttf", weight: "900" },
+    { path: "../fonts/Paperlogy-1Thin.woff2", weight: "100" },
+    { path: "../fonts/Paperlogy-2ExtraLight.woff2", weight: "200" },
+    { path: "../fonts/Paperlogy-3Light.woff2", weight: "300" },
+    { path: "../fonts/Paperlogy-4Regular.woff2", weight: "400" },
+    { path: "../fonts/Paperlogy-5Medium.woff2", weight: "500" },
+    { path: "../fonts/Paperlogy-6SemiBold.woff2", weight: "600" },
+    { path: "../fonts/Paperlogy-7Bold.woff2", weight: "700" },
+    { path: "../fonts/Paperlogy-8ExtraBold.woff2", weight: "800" },
+    { path: "../fonts/Paperlogy-9Black.woff2", weight: "900" },
   ],
   display: "swap",
 });
