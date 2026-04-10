388be5ceac64e9ed681c2667a124e281210dedd5
JHyun Ahn <jhyunahn226@gmail.com>
2026-04-10 13:06:43 +0900

    모임 및 인사이트 썸네일 이미지는 조금 더 크게 압축

--- lib/image-compression.ts
+++ lib/image-compression.ts
@@ -4,11 +4,11 @@ export const FILE_SIZE_LIMIT = 20 * 1024 * 1024; // 20MB
 
 export async function compressImage(
   file: File,
-  type: "thumbnail" | "detail",
+  type: "thumbnail" | "thumbnail-lg" | "detail",
 ): Promise<File> {
   const options = {
-    maxSizeMB: type === "thumbnail" ? 0.3 : 1,
-    maxWidthOrHeight: type === "thumbnail" ? 600 : 1200,
+    maxSizeMB: type === "detail" ? 1 : 0.3,
+    maxWidthOrHeight: type === "detail" ? 1200 : type === "thumbnail-lg" ? 800 : 600,
     useWebWorker: true,
     fileType: "image/webp",
   };
